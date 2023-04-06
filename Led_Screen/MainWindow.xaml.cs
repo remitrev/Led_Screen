5d45c70bf02f882e2c0d93861194a6ebd3817b17
remitrev <remi.trevisanuto@laposte.net>
2023-04-06 18:31:10 +0200

    Change screen Height

--- Led_Screen/MainWindow.xaml.cs
+++ Led_Screen/MainWindow.xaml.cs
@@ -30,6 +30,8 @@ namespace Led_Screen
 
         public ObservableCollection<BluetoothLEDevice> BluetoothLEDevices { get; } = new ObservableCollection<BluetoothLEDevice>();
 
+        public BluetoothLEDevice bluetoothDeviceSelected = null;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -89,6 +91,34 @@ namespace Led_Screen
             return false;
         }
 
+        private byte[] GetEnteteToSendContents(/*List content*/)
+        {
+            var bArr = new byte[64];
+            bArr[0] = 119;
+            bArr[1] = 97;
+            bArr[2] = 110;
+            bArr[3] = 103;
+            bArr[4] = 0;
+            bArr[5] = 0;
+            //TODO: getFlash bar[6]
+            //TODO: getMarquee bar[7]
+            //TODO: getModeAndSpeed [8 - 15]
+            //TODO: getMsgLength [16-31]
+            bArr[32] = 0;
+            bArr[33] = 0;
+            bArr[34] = 0;
+            bArr[35] = 0;
+            bArr[36] = 0;
+            bArr[37] = 0;
+            //TODO: getDate [38-43] peut etre mettre à 0, à tester
+            for (int i2 = 0; i2 < 19; i2++)
+            {
+                bArr[i2 + 44] = 0;
+            }
+            bArr[63] = 0;
+            return bArr;
+        }
+
         private async void Button_Click_1(object sender, RoutedEventArgs e)
         {
             await Start_SearchAsync();
@@ -96,7 +126,14 @@ namespace Led_Screen
 
         private void ListBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
         {
+            // Recupere le nom du Device
+            string selectedDeviceName = bluetoothDevicesListBox.SelectedItem as string;
 
+            // Recherche l'appareil BluetoothLEDevice correspondant dans la collection BluetoothLEDevices
+            BluetoothLEDevice selectedDevice = BluetoothLEDevices.FirstOrDefault(device => device.Name == selectedDeviceName);
+
+            // Affecte l'appareil sélectionné à la variable bluetoothDeviceSelected
+            bluetoothDeviceSelected = selectedDevice;
         }
     }
 }
