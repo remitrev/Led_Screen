becc1482bedda5740ce53b217602f08671cbb739
remitrev <remi.trevisanuto@laposte.net>
2023-05-15 14:03:31 +0200

    Transformation en mvvm

--- Led_Screen/MVVM/ViewModels/MainWindowViewModel.cs
+++ Led_Screen/MVVM/ViewModels/MainWindowViewModel.cs
@@ -3,51 +3,54 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
+using System.Windows.Threading;
 using Windows.Devices.Bluetooth;
 using Windows.Devices.Bluetooth.Advertisement;
 using Windows.Devices.Bluetooth.GenericAttributeProfile;
 using Windows.Storage.Streams;
 
-namespace Led_Screen
+namespace Led_Screen.MVVM.ViewModels
 {
-    /// <summary>
-    /// Logique d'interaction pour MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    class MainWindowViewModel
     {
+        public ObservableCollection<BluetoothLEDevice> BluetoothLEDevices { get; set; }
+        public BluetoothLEDevice bluetoothDeviceSelected { get; set; }
+
+        #region private data
         private BluetoothLEAdvertisementWatcher watcher;
-
         private Dictionary<string, byte[]> characterMapper;
+        private String UUID_CHARACTERISTICS_WRITE = "fee1";
+        private String UUID_SERVICE = "fee0";
+        private Guid serviceUUID = Guid.Parse("0000fee0-0000-1000-8000-00805f9b34fb");
+        private Guid characteristicsUUID = Guid.Parse("0000fee1-0000-1000-8000-00805f9b34fb");
+        #endregion private
 
-        public ObservableCollection<BluetoothLEDevice> BluetoothLEDevices { get; } = new ObservableCollection<BluetoothLEDevice>();
-
-        public BluetoothLEDevice bluetoothDeviceSelected = null;
-
-        public String UUID_CHARACTERISTICS_WRITE = "fee1";
-        public String UUID_SERVICE = "fee0";
-
-        public Guid serviceUUID = Guid.Parse("0000fee0-0000-1000-8000-00805f9b34fb");
-        public Guid characteristicsUUID = Guid.Parse("0000fee1-0000-1000-8000-00805f9b34fb");
-
-
-
-        public MainWindow()
+        public MainWindowViewModel()
         {
-            InitializeComponent();
+            BluetoothLEDevices = new ObservableCollection<BluetoothLEDevice>();
+            bluetoothDeviceSelected = null;
             InitWatcher();
             InitMappeur();
-            bluetoothDevicesListBox.ItemsSource = BluetoothLEDevices.Select(device => device.Name + " :" + device.BluetoothAddress);
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
         }
 
         #region Search Bluetooth Device
+        public async Task Start_SearchAsync()
+        {
+            this.watcher.Start();
+            await Task.Delay(2000);
+            this.watcher.Stop();
+            Debug.Print("Fin de recherche");
+        }
+
+        public void SelectBluetoothDevice(string address)
+        {
+            bluetoothDeviceSelected = BluetoothLEDevices.FirstOrDefault(device => device.BluetoothAddress == ulong.Parse(address)); ;
+        }
+
+        #region private method
         private void InitWatcher()
         {
             this.watcher = new BluetoothLEAdvertisementWatcher();
@@ -59,24 +62,17 @@ namespace Led_Screen
             this.watcher.Received += Watcher_Received;
         }
 
-        private async Task Start_SearchAsync()
-        {
-            this.watcher.Start();
-            await Task.Delay(2000);
-            this.watcher.Stop();
-            bluetoothDevicesListBox.ItemsSource = BluetoothLEDevices.Select(device => device.Name + " :" + device.BluetoothAddress);
-            Debug.Print("Fin de recherche");
-        }
+
 
         private async void Watcher_Received(
             BluetoothLEAdvertisementWatcher sender,
             BluetoothLEAdvertisementReceivedEventArgs args)
         {
-            var device = await BluetoothLEDevice.FromBluetoothAddressAsync(args.BluetoothAddress);            
+            var device = await BluetoothLEDevice.FromBluetoothAddressAsync(args.BluetoothAddress);
             if (device != null && !CheckBluetoothDevicesByAddress(device.BluetoothAddress) && device.Name.Equals("LSLED"))
             {
                 Debug.Print("Name :" + device.Name);
-                await Dispatcher.InvokeAsync(() => BluetoothLEDevices.Add(device));
+                await Application.Current.Dispatcher.InvokeAsync(() => BluetoothLEDevices.Add(device));
             }
         }
 
@@ -91,12 +87,25 @@ namespace Led_Screen
             }
             return false;
         }
-
+        #endregion
         #endregion
 
         #region Send Messages to devices
 
-        #region Get entete
+        public async Task SendToSelectedDevice(List<string> messages)
+        {
+            await SendToOneDeviceAsync(bluetoothDeviceSelected, messages);
+        }
+
+        public async Task SendToAllDevices(List<string> messages)
+        {
+            foreach (var device in BluetoothLEDevices)
+            {
+                await SendToOneDeviceAsync(device, messages);
+            }
+        }
+
+        #region Get headers
         private byte[] GetFirstEnteteToSendContents()
         {
             var bArr = new byte[16];
@@ -131,7 +140,7 @@ namespace Led_Screen
             // Transform the message length into 2 bytes
             bArr[0] = (byte)(messages[0].Length >> 8);  // MSB
             bArr[1] = (byte)messages[0].Length;         // LSB
-            if(messages[1] != "")
+            if (messages[1] != "")
             {
                 bArr[2] = (byte)(messages[1].Length >> 8);  // MSB
                 bArr[3] = (byte)messages[1].Length;         // LSB
@@ -159,11 +168,11 @@ namespace Led_Screen
                                     {
                                         bArr[14] = (byte)(messages[7].Length >> 8);  // MSB
                                         bArr[15] = (byte)messages[7].Length;         // LSB
-                                        
+
                                     }
                                     else
                                     {
-                                        bArr[14] = 0; 
+                                        bArr[14] = 0;
                                         bArr[15] = 0;
                                     }
                                 }
@@ -198,7 +207,8 @@ namespace Led_Screen
                             bArr[i2 + 2] = 0;
                         }
                     }
-                } else
+                }
+                else
                 {
                     for (int i2 = 4; i2 < 14; i2++)
                     {
@@ -238,9 +248,11 @@ namespace Led_Screen
         }
         #endregion
 
-        private async Task<bool> SendToOneDeviceAsync(BluetoothLEDevice device)
+        #region private methods
+        private async Task SendToOneDeviceAsync(BluetoothLEDevice device, List<string> messages)
         {
-            if (firstMessage.Text == "")
+            //TODO: transforme list string en plusieurs messages (voir quand model créer)
+            if (messages[0] == "")
             {
                 throw new Exception("No message to send");
             }
@@ -262,15 +274,6 @@ namespace Led_Screen
             List<byte[]> contents = new List<byte[]>();
             contents.Add(GetFirstEnteteToSendContents());
             //Concat messages
-            List<string> messages = new List<string>();
-            messages.Add(firstMessage.Text);
-            messages.Add(secondMessage.Text);
-            messages.Add(thirdMessage.Text);
-            messages.Add(forthMessage.Text);
-            messages.Add(_5Message.Text);
-            messages.Add(_6Message.Text);
-            messages.Add(_7Message.Text);
-            messages.Add(_8Message.Text);
             contents.Add(GetSecondEnteteToSendContents(messages));
             contents.Add(GetThirdEnteteToSendContents());
             contents.Add(GetForthEnteteToSendContents());
@@ -287,12 +290,11 @@ namespace Led_Screen
                 await characteristic.WriteValueAsync(buffer);
             }
             Debug.Print("Envoye");
-            return true;
         }
 
         private List<byte[]> transformMessage(List<String> messages)
         {
-            var res = new List<byte[]>();            
+            var res = new List<byte[]>();
             var temp = new List<byte[]>();
             //Concatene tout les messages
             var mess = messages[0] + messages[1] + messages[2] + messages[3] + messages[4] + messages[5] + messages[6] + messages[7];
@@ -302,7 +304,7 @@ namespace Led_Screen
             {
                 byte[] content = new byte[11];
                 //Init à 0 si on ne trouve pas le caractère dans le code
-                for(int i = 0; i<11; i++)
+                for (int i = 0; i < 11; i++)
                 {
                     content[i] = 0;
                 }
@@ -347,186 +349,11 @@ namespace Led_Screen
             }
             return res;
         }
+        #endregion
 
         #endregion
 
-
-        private async void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            errorMessage.Visibility = Visibility.Hidden;
-            BluetoothLEDevices.Clear();
-            await Start_SearchAsync();
-        }
-
-        private async void ListBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
-        {
-            errorMessage.Visibility = Visibility.Hidden;
-            // Recupere le nom du Device
-            string selectedDeviceString = bluetoothDevicesListBox.SelectedItem as string;
-
-            var tabDevice = selectedDeviceString.Split(':');
-
-            // Recherche l'appareil BluetoothLEDevice correspondant dans la collection BluetoothLEDevices
-            BluetoothLEDevice selectedDevice = BluetoothLEDevices.FirstOrDefault(device => device.BluetoothAddress == ulong.Parse(tabDevice[1]));
-
-            // Affecte l'appareil sélectionné à la variable bluetoothDeviceSelected
-            bluetoothDeviceSelected = selectedDevice;
-        }
-
-        private async void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-            //Reset label error
-            errorOnTryCatch.Content = "";
-            errorOnTryCatch.Visibility = Visibility.Hidden;
-            errorMessage.Visibility = Visibility.Hidden;
-
-            if (allLEDScreen.IsChecked == true)
-            {
-                if(BluetoothLEDevices.Count() == 0)
-                {
-                    errorMessage.Visibility = Visibility.Visible;
-                    return;
-                }
-                foreach (var device in BluetoothLEDevices)
-                {
-                    await SendToOneDeviceAsync(device);
-                    try
-                    {
-                        await SendToOneDeviceAsync(bluetoothDeviceSelected);
-                    } catch (Exception exc)
-                    {
-                        errorOnTryCatch.Content = exc.Message;
-                        errorOnTryCatch.Visibility = Visibility.Visible;
-                    }
-                    
-                }
-            }
-            else //Send to selected device
-            {
-                if(bluetoothDeviceSelected != null)
-                {
-                    try
-                    {
-                        await SendToOneDeviceAsync(bluetoothDeviceSelected);
-                    }
-                    catch (Exception exc)
-                    {
-                        errorOnTryCatch.Content = exc.Message;
-                        errorOnTryCatch.Visibility = Visibility.Visible;
-                    }
-                } else
-                {
-                    errorMessage.Visibility = Visibility.Visible;
-                    Debug.Print("pas de devices");
-                }
-            }
-        }   
-
-        private void CheckBox_Checked(object sender, RoutedEventArgs e)
-        {
-            errorMessage.Visibility = Visibility.Hidden;
-        }
-
-
-        private void TextBox1_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (firstMessage.Text == "")
-            {
-                secondMessage.IsReadOnly = true;
-                secondMessage.Text = "";
-                sendMessage.IsEnabled = false;
-            }
-            else
-            {
-                secondMessage.IsReadOnly = false;
-                sendMessage.IsEnabled = true;
-            }
-        }
-
-        private void TextBox2_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (secondMessage.Text == "")
-            {
-                thirdMessage.IsReadOnly = true;
-                thirdMessage.Text = "";
-            }
-            else
-            {
-                thirdMessage.IsReadOnly = false;
-            }
-        }
-
-        private void TextBox3_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (thirdMessage.Text == "")
-            {
-                forthMessage.IsReadOnly = true;
-                forthMessage.Text = "";
-            }
-            else
-            {
-                forthMessage.IsReadOnly = false;
-            }
-        }
-
-        private void TextBox4_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (forthMessage.Text == "")
-            {
-                _5Message.IsReadOnly = true;
-                _5Message.Text = "";
-            }
-            else
-            {
-                _5Message.IsReadOnly = false;
-            }
-        }
-
-        private void TextBox5_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (_5Message.Text == "")
-            {
-                _6Message.IsReadOnly = true;
-                _6Message.Text = "";
-            }
-            else
-            {
-                _6Message.IsReadOnly = false;
-            }
-        }
-
-        private void TextBox6_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (_6Message.Text == "")
-            {
-                _7Message.IsReadOnly = true;
-                _7Message.Text = "";
-            }
-            else
-            {
-                _7Message.IsReadOnly = false;
-            }
-        }
-
-        private void TextBox7_TextChanged(object sender, TextChangedEventArgs e)
-        {
-            if (_7Message.Text == "")
-            {
-                _8Message.IsReadOnly = true;
-                _8Message.Text = "";
-            }
-            else
-            {
-                _8Message.IsReadOnly = false;
-            }
-        }
-
-        private void TextBox8_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        #region init Mapper
+        #region Init Mapper
         private void InitMappeur()
         {
             this.characterMapper = new Dictionary<string, byte[]>();
@@ -635,4 +462,6 @@ namespace Led_Screen
         }
         #endregion
     }
+
+
 }
---
+++ Led_Screen/MVVM/Views/MainWindowView.xaml.cs
@@ -0,0 +1,221 @@
+﻿using Led_Screen.MVVM.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.Advertisement;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Storage.Streams;
+
+namespace Led_Screen
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindowView : Window
+    {
+        private MainWindowViewModel viewModel;
+        public MainWindowView()
+        {
+            InitializeComponent();
+            viewModel = new MainWindowViewModel();
+            DataContext = viewModel;
+            bluetoothDevicesListBox.ItemsSource = viewModel.BluetoothLEDevices.Select(device => device.Name + " :" + device.BluetoothAddress);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private async void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            errorMessage.Visibility = Visibility.Hidden;
+            viewModel.BluetoothLEDevices.Clear();
+            await viewModel.Start_SearchAsync();
+            bluetoothDevicesListBox.ItemsSource = viewModel.BluetoothLEDevices.Select(device => device.Name + " :" + device.BluetoothAddress);
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            errorMessage.Visibility = Visibility.Hidden;
+            // Recupere le nom et l'adresse du Device
+            string selectedDeviceString = bluetoothDevicesListBox.SelectedItem as string;
+
+            var tabDevice = selectedDeviceString.Split(':');
+
+            // Affecte l'appareil sélectionné à la variable bluetoothDeviceSelected
+            viewModel.SelectBluetoothDevice(tabDevice[1]);
+        }
+
+        private async void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            //Reset label error
+            errorOnTryCatch.Content = "";
+            errorOnTryCatch.Visibility = Visibility.Hidden;
+            errorMessage.Visibility = Visibility.Hidden;
+
+            if (allLEDScreen.IsChecked == true)
+            {
+                if (viewModel.BluetoothLEDevices.Count() == 0)
+                {
+                    errorMessage.Visibility = Visibility.Visible;
+                    return;
+                }
+
+                try
+                {
+                    await viewModel.SendToAllDevices(TransformMessagesTextBox());
+                }
+                catch (Exception exc)
+                {
+                    errorOnTryCatch.Content = exc.Message;
+                    errorOnTryCatch.Visibility = Visibility.Visible;
+                }
+                
+            }
+            else //Send to selected device
+            {
+                if(viewModel.bluetoothDeviceSelected != null)
+                {
+                    try
+                    {
+                        await viewModel.SendToSelectedDevice(TransformMessagesTextBox());
+                    }
+                    catch (Exception exc)
+                    {
+                        errorOnTryCatch.Content = exc.Message;
+                        errorOnTryCatch.Visibility = Visibility.Visible;
+                    }
+                } else
+                {
+                    errorMessage.Visibility = Visibility.Visible;
+                    Debug.Print("pas de devices");
+                }
+            }
+        }   
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            errorMessage.Visibility = Visibility.Hidden;
+        }
+
+
+        private void TextBox1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (firstMessage.Text == "")
+            {
+                secondMessage.IsReadOnly = true;
+                secondMessage.Text = "";
+                sendMessage.IsEnabled = false;
+            }
+            else
+            {
+                secondMessage.IsReadOnly = false;
+                sendMessage.IsEnabled = true;
+            }
+        }
+
+        private void TextBox2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (secondMessage.Text == "")
+            {
+                thirdMessage.IsReadOnly = true;
+                thirdMessage.Text = "";
+            }
+            else
+            {
+                thirdMessage.IsReadOnly = false;
+            }
+        }
+
+        private void TextBox3_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (thirdMessage.Text == "")
+            {
+                forthMessage.IsReadOnly = true;
+                forthMessage.Text = "";
+            }
+            else
+            {
+                forthMessage.IsReadOnly = false;
+            }
+        }
+
+        private void TextBox4_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (forthMessage.Text == "")
+            {
+                _5Message.IsReadOnly = true;
+                _5Message.Text = "";
+            }
+            else
+            {
+                _5Message.IsReadOnly = false;
+            }
+        }
+
+        private void TextBox5_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (_5Message.Text == "")
+            {
+                _6Message.IsReadOnly = true;
+                _6Message.Text = "";
+            }
+            else
+            {
+                _6Message.IsReadOnly = false;
+            }
+        }
+
+        private void TextBox6_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (_6Message.Text == "")
+            {
+                _7Message.IsReadOnly = true;
+                _7Message.Text = "";
+            }
+            else
+            {
+                _7Message.IsReadOnly = false;
+            }
+        }
+
+        private void TextBox7_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (_7Message.Text == "")
+            {
+                _8Message.IsReadOnly = true;
+                _8Message.Text = "";
+            }
+            else
+            {
+                _8Message.IsReadOnly = false;
+            }
+        }
+
+        private void TextBox8_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private List<string> TransformMessagesTextBox()
+        {
+            List<string> messages = new List<string>();
+            messages.Add(firstMessage.Text);
+            messages.Add(secondMessage.Text);
+            messages.Add(thirdMessage.Text);
+            messages.Add(forthMessage.Text);
+            messages.Add(_5Message.Text);
+            messages.Add(_6Message.Text);
+            messages.Add(_7Message.Text);
+            messages.Add(_8Message.Text);
+            return messages;
+        }
+    }
+}
