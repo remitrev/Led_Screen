c7c3d1cffe48beabe68fea9d874a5d4633c10fd3
remitrev <remi.trevisanuto@laposte.net>
2023-06-05 08:45:22 +0200

    update requete sql

--- Requête.sql
+++ Requête.sql
@@ -1,10 +1,12 @@
+CREATE USER 'visualstudio'@'localhost' IDENTIFIED BY 'visualstudio';
+
 CREATE DATABASE led_screen;
 
 GRANT ALL PRIVILEGES ON led_screen.* TO 'visualstudio'@'localhost' IDENTIFIED BY 'visualstudio';
 
 
 CREATE TABLE led_screen.message (
-  id CHAR(36) NOT NULL,message
+  id CHAR(36) NOT NULL,
   content VARCHAR(255) NOT NULL,
   tag VARCHAR(50) NOT NULL,
   createdDate DATETIME NOT NULL,
