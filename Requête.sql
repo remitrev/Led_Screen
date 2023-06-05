bca2925ecf72ad01a154ddead9d447de30c47b89
remitrev <remi.trevisanuto@laposte.net>
2023-06-05 08:41:23 +0200

    update sql requete

--- Requête.sql
+++ Requête.sql
@@ -1,5 +1,10 @@
-CREATE TABLE message (
-  id CHAR(36) NOT NULL,
+CREATE DATABASE led_screen;
+
+GRANT ALL PRIVILEGES ON led_screen.* TO 'visualstudio'@'localhost' IDENTIFIED BY 'visualstudio';
+
+
+CREATE TABLE led_screen.message (
+  id CHAR(36) NOT NULL,message
   content VARCHAR(255) NOT NULL,
   tag VARCHAR(50) NOT NULL,
   createdDate DATETIME NOT NULL,
